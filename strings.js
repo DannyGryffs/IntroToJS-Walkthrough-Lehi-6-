0a54e64c595037c2529c5774cb9cc8c053577566
Daniel Griffin <dgriffinpv@gmail.com>
2023-11-28 11:29:39 -0700

    strings file complete

---
+++ strings.js
@@ -0,0 +1,16 @@
+// const hello = "Hello, World!"
+
+// let stringWithSingleQuotes = 'Single quotes work too';
+
+// console.log(hello[0]);
+
+// const ball = 'ball'
+// const templateString = `Foot${ball}`
+
+// console.log(templateString)
+
+let yourName = 'Steve';
+
+console.log(`Hi, ${yourName}`)
+
+console.log("Hi " + yourName + "!")
