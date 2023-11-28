1ea88653314a70f65e15423eb6211e486e87ef62
Daniel Griffin <dgriffinpv@gmail.com>
2023-11-28 12:42:15 -0700

    added numbers, index, and calc

---
+++ calc.js
@@ -0,0 +1,23 @@
+//calc project
+
+
+let operand1 = process.argv[2]
+let operator = process.argv[3]
+let operand2 = process.argv[4]
+
+// console.log(operand1)
+// console.log(operator)
+// console.log(operand2)
+
+if(operator === '+'){
+    console.log(Number(operand1) + Number(operand2))
+}else if(operator === '-'){
+    console.log(Number(operand1) - Number(operand2))
+}else if(operator === '*'){
+    console.log(operand1 * operand2)
+}else if(operator === '/' && operand2 === '0'){
+    console.log("Can't divide by 0")
+}
+else if(operator === '/'){
+console.log(operand1 / operand2)
+}
